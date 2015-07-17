933291549d8b468fd09e8e410e215eb903f5ccd0
denis280883 <denis_ge@yahoo.fr>
2015-07-17 22:34:08 +0200

    EssaiVisualStudio2013

---
+++ Optisoft/Optisoft/glasses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optisoft
+{
+    class glasses
+    {
+        private String _make;
+        private String _model;
+        private String _color;
+        private String _type;
+        private String _Materials;
+
+
+        public String Make
+        {
+            get 
+            {
+                return this._make;
+            }
+
+            set
+            {
+                this._make = value;
+            }
+        }
+
+
+
+    }
+}
